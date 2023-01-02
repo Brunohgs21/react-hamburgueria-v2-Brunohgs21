9c0f707522e37221e97fb1ca1e948e626fd315f9
Brunohgs21 <brunohgs21@outlook.com>
2023-01-02 14:34:22 -0300

    fix: extensão

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Img from "../../Assets/Component1.png";
 import FormRegister from "../../Components/FormRegister/FormRegister";
-import { Div } from "../../Components/FormRegister/style";
+import { Div } from "../../Components/FormRegister/style.js";
 
 const Register = () => {
   return (
