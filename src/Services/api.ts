6cd770214fb348fc104eae171c4d85045ddead20
Brunohgs21 <brunohgs21@outlook.com>
2024-05-01 17:05:15 -0300

    fix

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://hamburgueria-kenzie-v2.herokuapp.com/",
+  baseURL: "https://hbv2.alextoledo.com.br/",
   timeout: 5000,
 });
+// https://hamburgueria-kenzie-v2.herokuapp.com
